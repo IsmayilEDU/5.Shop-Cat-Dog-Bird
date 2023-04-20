b8eb2489442ef59ef32d1b396d0040084e4c00bd
ismayilkerimzade <ismayil.programmer@gmail.com>
2023-04-20 10:24:29 +0400

    first

---
+++ Shop - Cat, Dog, Bird/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop___Cat__Dog__Bird
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
